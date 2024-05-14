ffaf4deec24c2a9d3446ce87c02b2a6137938337
Alexandros Kasiotakis <akasiota@student.codam.nl>
2024-05-14 14:52:53 +0200

    small cleanup after merging with main

--- src/main.c
+++ src/main.c
@@ -25,47 +25,47 @@ void move(void *param)
 		moves(data, -1, 1);
 }
 
-int	map_build(t_data *data)
-{
-	data->player = mlx_new_image(data->mlx, 1500, 1000);
-	ft_memset(data->player->pixels, 255, 1500*1000*4);
-	mlx_image_to_window(data->mlx, data->player, 0, 0);
-}
+// int	map_build(t_data *data)
+// {
+// 	data->player = mlx_new_image(data->mlx, 1500, 1000);
+// 	ft_memset(data->player->pixels, 255, 1500*1000*4);
+// 	mlx_image_to_window(data->mlx, data->player, 0, 0);
+// }
 
-int	map_init(t_data *data)
-{
-	data->map = calloc(10, sizeof(char *)); // init the map
-	data->map[0] = strdup("1111111111111111111111111"); //fill the map
-	data->map[1] = strdup("1000000000000000000100001");
-	data->map[2] = strdup("1001000000000P00000000001");
-	data->map[3] = strdup("1001000000000000001000001");
-	data->map[4] = strdup("1001000000000000001000001");
-	data->map[5] = strdup("1001000000100000001000001");
-	data->map[6] = strdup("1001000000000000001000001");
-	data->map[7] = strdup("1001000000001000001000001");
-	data->map[8] = strdup("1111111111111111111111111");
-	data->map[9] = NULL;
-	// data->p_y = 3; // player y position in the map
-	// data->p_x = 14; // player x position in the map
-	// data->w_map = 25; // map width
-	// data->h_map = 9; // map height
-	data->player = mlx_new_image(data->mlx, 10, 10);
-	ft_memset(data->player->pixels, 255, 400);
-	mlx_image_to_window(data->mlx, data->player, 750, 500);
-	return (0);
-}
+// int	map_init(t_data *data)
+// {
+// 	data->map = calloc(10, sizeof(char *)); // init the map
+// 	data->map[0] = strdup("1111111111111111111111111"); //fill the map
+// 	data->map[1] = strdup("1000000000000000000100001");
+// 	data->map[2] = strdup("1001000000000P00000000001");
+// 	data->map[3] = strdup("1001000000000000001000001");
+// 	data->map[4] = strdup("1001000000000000001000001");
+// 	data->map[5] = strdup("1001000000100000001000001");
+// 	data->map[6] = strdup("1001000000000000001000001");
+// 	data->map[7] = strdup("1001000000001000001000001");
+// 	data->map[8] = strdup("1111111111111111111111111");
+// 	data->map[9] = NULL;
+// 	// data->p_y = 3; // player y position in the map
+// 	// data->p_x = 14; // player x position in the map
+// 	// data->w_map = 25; // map width
+// 	// data->h_map = 9; // map height
+// 	data->player = mlx_new_image(data->mlx, 10, 10);
+// 	ft_memset(data->player->pixels, 255, 400);
+// 	mlx_image_to_window(data->mlx, data->player, 750, 500);
+// 	return (0);
+// }
 
 int main(int argc, char *argv[])
 {
 	t_data	data;
-	map_init(&data);
-	data.mlx = mlx_init(1500, 1000, "cub3D", true);
-	map_build(&data);
-	// mlx_key_hook(data.mlx, move, &data);
-	mlx_loop_hook(data.mlx, move, &data);
-	mlx_loop(data.mlx);
-	mlx_terminate(data.mlx);
-=======
+
+	// map_init(&data);
+	// data.mlx = mlx_init(1500, 1000, "cub3D", true);
+	// map_build(&data);
+	// // mlx_key_hook(data.mlx, move, &data);
+	// mlx_loop_hook(data.mlx, move, &data);
+	// mlx_loop(data.mlx);
+	// mlx_terminate(data.mlx);
 	// map_init();
 	data_init(&data);
 	validate_args(&data, argc, argv);
