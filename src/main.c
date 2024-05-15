c96acd74371a895e3c186bfbcaaed3a282c32d01
Selim Yildirim <seyildir@f1r1s13.codam.nl>
2024-05-15 13:37:02 +0200

    codam

--- src/main.c
+++ src/main.c
@@ -20,31 +20,51 @@ void move(void *param)
 	if (mlx_is_key_down(data->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(data->mlx);
 	if (mlx_is_key_down(data->mlx, MLX_KEY_W))
-		moves(data, -1, VERTICAL);
+		moves(data, -5, VERTICAL);
 	if (mlx_is_key_down(data->mlx, MLX_KEY_S))
-		moves(data, 1, VERTICAL);
+		moves(data, 5, VERTICAL);
 	if (mlx_is_key_down(data->mlx, MLX_KEY_D))
-		moves(data, 1, HORIZONTAL);
+		moves(data, 5, HORIZONTAL);
 	if (mlx_is_key_down(data->mlx, MLX_KEY_A))
-		moves(data, -1, HORIZONTAL);
+		moves(data, -5, HORIZONTAL);
+}
+
+void make_color(mlx_image_t *p, int x, int y)
+{
+	int i = 0;
+	while (i < (x*y*(int)sizeof(uint32_t)))
+	{
+		p->pixels[i++] = 255;
+		p->pixels[i++] = 255;
+		p->pixels[i++] = 0;
+		p->pixels[i++] = 255;
+	}
+	
 }
 
 int	map_build(t_data *data)
 {
-	data->player = mlx_new_image(data->mlx, 10, 10);
-	ft_memset(data->player->pixels, 255, 400);
+	int px;
+	int py;
+
+	px = 15;
+	py = 15;
+	data->player = mlx_new_image(data->mlx, px, py);
+	make_color(data->player, px, py);
 	mlx_image_to_window(data->mlx, data->player, 750, 500);
 }
 
 int main(int argc, char const *argv[])
 {
 	t_data	data;
+
 	// map_init();
 	printf("%ld\n", sizeof(int32_t));
 	data.mlx = mlx_init(1500, 1000, "cub3D", true);
 	map_build(&data);
 	// mlx_key_hook(data.mlx, move, &data);
 	mlx_loop_hook(data.mlx, move, &data);
+	// mlx_loob_hook(data.mlx, ray_cast, );
 	mlx_loop(data.mlx);
 	mlx_terminate(data.mlx);
 	return 0;
